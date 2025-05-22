7f655957c0bcebc4b607b2adc9e32975e5d8ddf6
Uberney2 <uzapatap@gmail.com>
2025-05-21 23:12:52 -0500

    fix updated process

--- src/context/Processes/application/ProcessUpdater.ts
+++ src/context/Processes/application/ProcessUpdater.ts
@@ -4,10 +4,14 @@ import { Process, ProcessRepository } from '../domain';
 import { ProcessNotUpdated, ProcessNotFound } from '../domain/exceptions';
 import { BaseDimension } from '../../Shared/domain/dimension/application/BaseDimension';
 import { ProjectNotFound } from '../../Projects/domain/exceptions';
+import { Inject } from '@nestjs/common';
 
 export class ProcessUpdater extends BaseDimension {
   constructor(
+    @Inject('ProjectRepository')
     protected readonly projectRepository: ProjectRepository,
+
+    @Inject('ProcessRepository')
     protected readonly processRepository: ProcessRepository
   ) {
     super(processRepository);
